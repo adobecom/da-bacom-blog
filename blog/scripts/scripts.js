308a194377c20da2623631d683c14830bdc77bc0
Chris Millar <cmillar@adobe.com>
2024-11-15 13:06:45 -0700

    Typed Query Index support
    * Forked Milo article-feed block

--- blog/scripts/scripts.js
+++ blog/scripts/scripts.js
@@ -18,7 +18,7 @@ export function setLibs(location) {
   return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
 }
 
-const LIBS = setLibs(window.location);
+export const LIBS = setLibs(window.location);
 
 /**
  * Builds a block DOM Element from a two dimensional array
