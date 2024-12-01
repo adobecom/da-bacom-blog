1700c9253c0e464dff4f02459d1ac328682b15a6
Chris Millar <cmillar@adobe.com>
2024-12-01 14:24:45 +0100

    Add mock details. Next gen sidekick doesnt seem to have them.

--- blog/scripts/sidekick.js
+++ blog/scripts/sidekick.js
@@ -1,10 +1,18 @@
-// loadScript and loadStyle are passed in to avoid circular dependencies
+const TEMP_PROJ_CONFIG = {
+  host: 'business.adobe.com',
+  project: 'BACOM-BLOG',
+  ref: 'main',
+  repo: 'da-bacom-blog',
+  owner: 'adobecom',
+};
+
 export default function init({ loadScript, loadStyle }) {
   // manifest v3
   const sendToCaasListener = async (e) => {
-    const { host, project, ref: branch, repo, owner } = e.detail.data.config;
+    const { host, project, ref: branch, repo, owner } = e.detail?.data.config || TEMP_PROJ_CONFIG;
     // eslint-disable-next-line import/no-unresolved
     const { sendToCaaS } = await import('https://milo.adobe.com/tools/send-to-caas/send-to-caas.js');
+
     sendToCaaS({ host, project, branch, repo, owner }, loadScript, loadStyle);
   };
 
