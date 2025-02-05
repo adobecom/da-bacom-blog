3e8022a3f1443cf6d52d2be61a52f880619b4a8f
Jason Slavin <slavin@adobe.com>
2025-02-05 10:15:26 -0800

    Removing code

--- blog/scripts/sidekick.js
+++ blog/scripts/sidekick.js
@@ -1,23 +1,23 @@
-const TEMP_PROJ_CONFIG = {
-  host: 'business.adobe.com',
-  project: 'BACOM-BLOG',
-  ref: 'main',
-  repo: 'da-bacom-blog',
-  owner: 'adobecom',
-};
+// const TEMP_PROJ_CONFIG = {
+//   host: 'business.adobe.com',
+//   project: 'BACOM-BLOG',
+//   ref: 'main',
+//   repo: 'da-bacom-blog',
+//   owner: 'adobecom',
+// };
 
-export default function init({ loadScript, loadStyle }) {
-  // manifest v3
-  const sendToCaasListener = async (e) => {
-    const { host, project, ref: branch, repo, owner } = e.detail?.data.config || TEMP_PROJ_CONFIG;
-    // eslint-disable-next-line import/no-unresolved
-    const { sendToCaaS } = await import('https://milo.adobe.com/tools/send-to-caas/send-to-caas.js');
+// export default function init({ loadScript, loadStyle }) {
+//   // manifest v3
+//   const sendToCaasListener = async (e) => {
+//     const { host, project, ref: branch, repo, owner } = e.detail?.data.config || TEMP_PROJ_CONFIG;
+//     // eslint-disable-next-line import/no-unresolved
+//     const { sendToCaaS } = await import('https://milo.adobe.com/tools/send-to-caas/send-to-caas.js');
 
-    sendToCaaS({ host, project, branch, repo, owner }, loadScript, loadStyle);
-  };
+//     sendToCaaS({ host, project, branch, repo, owner }, loadScript, loadStyle);
+//   };
 
-  const sk = document.querySelector('aem-sidekick, helix-sidekick');
+//   const sk = document.querySelector('aem-sidekick, helix-sidekick');
 
-  // Add plugin listeners here
-  sk.addEventListener('custom:dx-send-to-caas', sendToCaasListener);
-}
+//   // Add plugin listeners here
+//   sk.addEventListener('custom:dx-send-to-caas', sendToCaasListener);
+// }
