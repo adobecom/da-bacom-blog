b81e4936e5b585298ca81ecf934984c1fe252af4
Ratko Zagorac <ratkozagorac@gmail.com>
2026-04-17 15:46:35 +0200

    MWPW-192736: whitelist milolibs branch param to block DOM XSS

--- blog/scripts/scripts.js
+++ blog/scripts/scripts.js
@@ -14,6 +14,7 @@ export function setLibs(location) {
   const { hostname, search } = location;
   if (!['.aem.', '.hlx.', '.stage.', 'local'].some((i) => hostname.includes(i))) return '/libs';
   const branch = new URLSearchParams(search).get('milolibs') || 'main';
+  if (!/^[a-zA-Z0-9_-]+$/.test(branch)) throw new Error('Invalid branch name.');
   if (branch === 'local') return 'http://localhost:6456/libs';
   return branch.includes('--') ? `https://${branch}.aem.live/libs` : `https://${branch}--milo--adobecom.aem.live/libs`;
 }
